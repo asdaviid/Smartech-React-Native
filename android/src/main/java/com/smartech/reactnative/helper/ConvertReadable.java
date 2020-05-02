3752d1568f7ae356d3e348eba82a501efc4847d4
David Alamu <aestrex@gmail.com>
2020-05-02 07:15:25 +0100

    fix imports

--- android/src/main/java/com/smartech/reactnative/helper/ConvertReadable.java
+++ android/src/main/java/com/smartech/reactnative/helper/ConvertReadable.java
@@ -8,6 +8,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ConvertReadable {
 
     public static JSONObject convertMapToJson(ReadableMap readableMap) throws JSONException {
